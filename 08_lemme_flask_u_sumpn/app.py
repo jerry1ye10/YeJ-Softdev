f9ca355715aa71fad65147aa93d25ebaa457a534
jerry1ye10 <jerry1ye10@gmail.com>
2018-09-19 12:43:47 -0400

    pushing Flask app with 3 routes

---
+++ 08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,22 @@
+#Jerry Ye
+#SoftDev1 pd7
+#K08 -- Fill Yer Flask
+#2018-09-20
+from flask import Flask #imports class Flask
+app = Flask(__name__)#Creates an instance of Flask
+
+@app.route('/')#Defines index
+def hello_world():
+    return "Hello world!"
+
+@app.route('/awesome')#Defines 2nd route
+def hello_awesome():
+    return "Hello awesome!"
+
+@app.route('/amazing')#Defines 3rd route
+def hello_amazing():
+    return "Hello Amazing!"
+
+if (__name__) == "__main__":#if this file is run directly then the Flask app will run
+    app.debug = True
+    app.run()
